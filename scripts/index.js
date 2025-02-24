9be483c47371a266b75d284b84ff3bd009d57644
dylan jennings <dylanfjennings@outlook.com>
2025-02-24 01:34:58 -0500

    add Java

---
+++ scripts/index.js
@@ -0,0 +1,25 @@
+const initialCards = [
+  {
+    name: "lobster",
+    link: "https://unsplash.com/photos/lobster-on-white-saucer-YeOMRwgvPv4",
+  },
+  {
+    name: "oyster",
+    link: "https://unsplash.com/photos/person-in-black-leather-jacket-holding-silver-fork-7q6pW_OJFx8",
+  },
+  { name: "bass", link: "https://unsplash.com/photos/A34kA5Xi_b4" },
+  {
+    name: "tuna",
+    link: "https://unsplash.com/photos/gray-tuna-fish-BRnnJ6vVNdY",
+  },
+  {
+    name: "Crab",
+    link: "https://unsplash.com/photos/brown-and-white-crab-J3_ylL4__2U",
+  },
+  {
+    name: "Shrimp",
+    link: "https://unsplash.com/photos/pile-of-shrimps-r4QVfQtytQ4",
+  },
+];
+
+console.log(initialCards);
